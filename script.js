b09d7948a026cc7b06086977348c0812f654830b
erik4maia <145574046+erik4maia@users.noreply.github.com>
2025-04-01 08:44:42 -0300

    01/04

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function () {
          tamanhoAtualFonte += 0.1;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+   });
+
+
+
+
 
 });
